8b38935bdcbb3317ea45420d148b0fb41c8345b8
germans208 <german.settino@gmail.com>
2020-06-26 18:49:24 -0300

    Se agrego el archivo .gitignore y modificacion de rutas de carpetas

--- src/app.js
+++ src/app.js
@@ -4,24 +4,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ************ Route System require and use() ************
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// ************ WRITE YOUR CODE FROM HERE ************
+app.use('/', indexRouter); // Rutas main
+app.use('/users', usersRouter);// Rutas /users
 
+
+
+
+// ************ catch 404 and forward to error handler ************
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
