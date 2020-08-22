cbc53577afe8b46d47175ef4de3958ecbd9db33b
Lis <someEmail>
2020-08-22 10:43:56 -0300

    fix/Arreglo Seeder de producto

--- src/database/seeders/20200810194334-single_products.js
+++ src/database/seeders/20200810194334-single_products.js
@@ -6,16 +6,16 @@ module.exports = {
     return queryInterface.bulkInsert('products', [{
       name: 'Ajedrez',
       material_id: 2,
-      color_id: "Celeste",
+      color_id: 1,
       category_id: 0,
       age: 3,
-      discount: '',
-      price: 0,
+      discount: 0,
+      price: 1200,
       image: 'imagen2.jpg',
-      quantity:'',
-      details:'',
-      createdAt: new Date(),
-      updatedAt: new Date()
+      stock:3,
+      description:'Descripcion de un ajedrez de color celeste.',
+      created_at: new Date(),
+      updated_at: new Date()
     }]);
   },
 
