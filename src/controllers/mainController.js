0768385839ea085ff3ecbf1d537599905e65b16d
Antonio1965 <saburo_tsutsumi@yahoo.com.ar>
2020-08-23 08:49:10 -0300

    Se completa parte de contactos (footer)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -7,13 +7,24 @@ const mainController = {
 			res.render('index', { products : products });
 	     })	
 	},
+	terminos: (req, res)=> {
+		res.render('terminos-condiciones', {breadcrumbs: req.breadcrumbs});
+	},
 	politics: (req, res) => {
 		res.render('politics', {breadcrumbs: req.breadcrumbs});
 	},
+	informacion: (req, res) => {
+		res.render('informacion', {breadcrumbs: req.breadcrumbs});
+	},
 	sucursales: (req, res) => {
 		res.render('sucursales', {breadcrumbs: req.breadcrumbs});
+	},
+	devoluciones: (req, res) => {
+		res.render('devoluciones', {breadcrumbs: req.breadcrumbs});
+	},
+	pagos: (req, res) => {
+		res.render('pagos', {breadcrumbs: req.breadcrumbs});
 	}
-
 };
 
 module.exports = mainController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -9,10 +9,20 @@ const breadcrumbs = require('../middlewares/breadcrumbsMiddleware');
 
 router.get('/', mainController.root); /* GET - home page */
 
+//-----FOOTER- Contactatos------//
+
+router.get('/terminos', breadcrumbs.breadcrumbsMiddleware(), mainController.terminos );
+
 router.get('/politics', breadcrumbs.breadcrumbsMiddleware(), mainController.politics );
+
+router.get('/informacion', breadcrumbs.breadcrumbsMiddleware(), mainController.informacion );
+
 
 router.get('/sucursales', breadcrumbs.breadcrumbsMiddleware(), mainController.sucursales );
 
+router.get('/devoluciones', breadcrumbs.breadcrumbsMiddleware(), mainController.devoluciones );
+
+router.get('/pagos', breadcrumbs.breadcrumbsMiddleware(), mainController.pagos );
 
 
 module.exports = router;
