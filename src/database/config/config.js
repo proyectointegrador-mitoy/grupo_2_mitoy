0953bab946eda475dc6ceb392d81d7934a5e65d2
germans208 <german.settino@gmail.com>
2020-08-25 15:57:04 -0300

    update env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 
-
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
-    "password": "root",
+    "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "port": 8889,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
